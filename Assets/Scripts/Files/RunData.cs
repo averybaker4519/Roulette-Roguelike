b81aa23afbd4f5f268b4683e476e061cd55588f8
tim-kashani <tim@fireapples.com>
2026-01-29 17:56:42 -0800

    Update RunData.cs

--- Assets/Scripts/Files/RunData.cs
+++ Assets/Scripts/Files/RunData.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class RunData
 {
+    // x is number
+    // y is color (0 is red, 1 is black, 2 is green)
+    // z is custom data???
     public Vector3Int[] pockets;
 
     public int cash;
