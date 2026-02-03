5d6e6b519adb1db908bb953d44d2ef6526f97756
Avery Baker <averybaker4519@gmail.com>
2026-02-03 14:13:51 -0800

    created betting type enum

---
+++ Assets/Scripts/Betting/BetType.cs
@@ -0,0 +1,9 @@
+public enum BetType
+{
+    Straight,   // single number
+    RedBlack,   // color
+    EvenOdd,    // even / odd
+    LowHigh,    // 1-18 / 19-36
+    Dozen,      // 1-12, 13-24, 25-36
+    Column      // column 1/2/3
+}
